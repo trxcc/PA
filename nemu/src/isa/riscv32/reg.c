953551c1cab4aca9ee610464d0eb824e08b08a46
tracer-ics2022 <tracer@njuics.org>
2022-09-27 19:28:09 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:28:09 up 17 min,  5 users,  load average: 0.07, 0.05, 0.07

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,6 +25,10 @@ const char *regs[] = {
 
 void isa_reg_display() {
   Log("hhh");
+  for (int i = 0; i < 32; i++){
+    printf("%s", reg_name(i, 8));
+    puts("\n");
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
