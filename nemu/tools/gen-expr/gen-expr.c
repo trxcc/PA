edd0ce66fa6e525a63e55e678489ad3ebe51c941
211300082-Tan Rongxi <211300082@smail.nju.edu.cn>
2022-09-29 23:59:14 +0800

    Today's journey of PA is not so smooth, with the heavy courses. Generator is done. Next task is to link generator with the expr function.

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -26,26 +26,106 @@ static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
 "int main() { "
-"  unsigned result = %s; "
+"  unsigned result = (unsigned)%s; "
 "  printf(\"%%u\", result); "
 "  return 0; "
-"}";
+"}" ;
 
-static void gen_rand_expr() {
-  buf[0] = '\0';
+int pos = 0;
+int token_len = 0;
+
+static uint32_t choose(uint32_t n) {
+  return rand() % n;
+}
+
+static void gen_num() {
+  int num = rand() % 3500, rec = num;
+  char tmp[5], e[5]; int i = 0; 
+  do{
+    tmp[i++] = (char)((num % 10) + '0');
+    //printf("%d %d\n", i, num);
+    num /= 10;
+  }while(num);
+  for (int j = 0; j < i; j++) { e[j] = tmp[i-1-j]; }
+  //printf("%d %s\n", rec, e);
+  strncpy(&buf[pos], &e[0], strlen(e));
+  //printf("%d - %ld  %s\n", pos, pos+strlen(e), e);
+  pos += strlen(e);
+}
+
+static void gen(char ch){
+  buf[pos++] = ch;
+  //printf("%d %c\n", pos-1, ch);
+  return;
+}
+
+static void gen_rand_op(){
+  char op[] = {'+', '-', '*', '/'};
+  buf[pos++] = op[choose(4)];
+  //printf("%d %c\n", pos-1, buf[pos-1]);
+  return;
+}
+
+static int chk(){
+  printf("%s\n", buf);
+  for (int i = 0; i < pos; i++){
+    if (buf[i] == '/' || buf[i] == '*' || buf[i] == '+' || buf[i] == '-') {
+      int k = i + 1;
+      while(buf[k] == ' ') ++k;
+      if (i == pos - 1 || buf[k] == '\0' || buf[k] == ')' || buf[k] == '+' || buf[k] == '-' || buf[k] == '*' || buf[k] == '/') { 
+        if (i == pos-1) { buf[i] = '\0'; }
+        else for (int j = i; j < pos-1; j++) { buf[j] = buf[j+1]; }
+        buf[pos-1] = '\0'; --pos; --i;
+      }
+    }
+    //if (buf[i] == '/') printf("%c %c %d\n", buf[i], buf[i+1], i == pos-1);
+  }
+  return 1;
+}
+
+static void gen_space(){
+  int num = rand() % 4;
+  for (int i = 0; i < num; i++) { buf[pos+i] = ' ';/* printf("%d is space \n", pos+i);*/ }
+  pos += num;
+  return;
+}
+
+static void gen_rand_expr(int flag) {
+  //buf[0] = '\0';
+  if (flag) {
+    do{
+      memset(buf, '\0', sizeof(buf));
+      pos = 0, token_len = 0;
+      switch (choose(3)) {
+        case 0: gen_space(); gen_num(); gen_space(); ++token_len; break;
+        case 1: gen_space(); gen('('); gen_space(); gen_rand_expr(0); gen_space(); gen(')'); gen_space(); token_len += 2; break; 
+        default: gen_space(); gen_rand_expr(0); gen_space(); gen_rand_op(); gen_space(); gen_rand_expr(0); gen_space(); ++token_len; break;
+      }
+    } while(token_len > 32 || pos > 65535 || !chk());
+  }
+  else {
+    switch(choose(3)){
+      case 0: gen_num(); ++token_len; break;
+      case 1: gen('('); gen_rand_expr(0); gen(')'); token_len += 2; break;
+      default: gen_rand_expr(0); gen_rand_op(); gen_rand_expr(0); ++token_len; break;
+    }
+  }
+  return;
 }
 
 int main(int argc, char *argv[]) {
   int seed = time(0);
   srand(seed);
   int loop = 1;
+  buf[0] = '\0';
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
   int i;
   for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
-
+    gen_rand_expr(1);
+    assert(buf[0] != '\0');
+    printf("%s\n", buf);
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen("/tmp/.code.c", "w");
