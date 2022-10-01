e431eeb8bb63f7830d260b8f56f7caa27f1dfd4f
tracer-ics2022 <tracer@njuics.org>
2022-10-01 17:45:49 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:45:49 up 31 min,  5 users,  load average: 0.02, 0.08, 0.09

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -20,6 +20,8 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
+extern word_t expr(char *e, bool success);
+
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
