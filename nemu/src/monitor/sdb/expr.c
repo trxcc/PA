6b09e61d2f78be9ea1360b687597a1d714265ca9
tracer-ics2022 <tracer@njuics.org>
2022-10-02 23:51:10 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:51:10 up  1:48,  4 users,  load average: 0.00, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -98,9 +98,9 @@ static bool make_token(char *e) {
   nr_token = 0;
 
   while (e[position] != '\0') {
-    assert(0);
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+        assert(0);
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
         assert(0);
