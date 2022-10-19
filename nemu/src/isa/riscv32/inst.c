1a45fd315856345d77646f0b0c8e81d40a8a0813
tracer-ics2022 <tracer@njuics.org>
2022-10-19 10:20:07 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:20:07 up 40 min,  5 users,  load average: 0.18, 0.18, 0.12

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -41,7 +41,7 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
   int rs2 = BITS(i, 24, 20);
   *dest = rd;
   switch (type) {
-    case TYPE_I: src1R();          immI(); break;
+    case TYPE_I: src1R();          immI(); assert(0); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
     case TYPE_J:                   immJ(); break;
