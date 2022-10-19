b287db4b8ba4c23e4e651b8c829d3177aceb51ec
tracer-ics2022 <tracer@njuics.org>
2022-10-19 10:48:31 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:48:31 up  1:09,  5 users,  load average: 0.02, 0.07, 0.06

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -80,8 +80,8 @@ static void execute(uint64_t n) {
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
-    trace_and_difftest(&s, cpu.pc);
     assert(0);
+    trace_and_difftest(&s, cpu.pc);
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
   }
