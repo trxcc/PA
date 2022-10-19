49cdc34384740ea6dffc5e1f2af2cf75a5e885c7
tracer-ics2022 <tracer@njuics.org>
2022-10-19 10:35:29 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:35:29 up 56 min,  5 users,  load average: 0.05, 0.03, 0.05

--- nemu/include/cpu/decode.h
+++ nemu/include/cpu/decode.h
@@ -93,11 +93,10 @@ finish:
   if (((INSTPAT_INST(s) >> shift) & mask) == key) { \
     INSTPAT_MATCH(s, ##__VA_ARGS__); \
     goto *(__instpat_end); \
-    assert(0);\
   } \
 } while (0)
 
 #define INSTPAT_START(name) { const void ** __instpat_end = &&concat(__instpat_end_, name);
-#define INSTPAT_END(name)   concat(__instpat_end_, name): ; }
+#define INSTPAT_END(name)   concat(__instpat_end_, name): ; assert(0); }
 
 #endif
