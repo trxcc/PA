587a9c1fdc7970564beb7b52db8bac255c37b832
tracer-ics2022 <tracer@njuics.org>
2022-10-25 20:00:01 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:00:01 up  4:41,  4 users,  load average: 0.08, 0.10, 0.06

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,6 +15,40 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  va_list vl;
+  va_start(vl, fmt);
+  int i = 0;
+  const char *p = fmt;
+  while (*p != '\0') {
+    if (*p != '%') {
+      out[i++] = *p; ++p;
+    }
+    else {
+      ++p;
+      switch (*p) {
+        case 'd':
+          int vaint = va_arg(vl, int);
+          int len = 0, tmp = vaint;
+          while (tmp) ++len, tmp /= 10;
+          for (int j = i + len; j >= 0; j--){
+            out[j] = (char)((vaint % 10) - '0');
+            vaint /= 10;
+          } 
+          i += len; break;
+        case 's':
+          char *vachars = va_arg(vl, char *);
+          while (*vachars != '\0') {
+            out[i++] = *vachars;
+            vachars++;
+          }
+          break;
+        default: out[i++] = *p; break;
+      }
+      ++p;
+    }
+  }
+  out[i] = '\0';
+  return i;
   panic("Not implemented");
 }
 
