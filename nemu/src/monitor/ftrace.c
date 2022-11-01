9cb4db2c08772f278094720729e403464d1ba011
tracer-ics2022 <tracer@njuics.org>
2022-11-01 23:38:26 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:38:26 up 11:11,  4 users,  load average: 0.09, 0.06, 0.02

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -21,9 +21,11 @@ FuncNode funcnode[MAX_NODE];
 static int cnt = 0;
 
 void getStrTable (char *filepath) {
-  fp = stdout;
+//  fp = stdout;
   if (filepath != NULL) {
+    printf("hh\n");
     FILE *tmp_fp = fopen(filepath, "w");
+    printf("cc\n");
     Assert(tmp_fp, "Can not open '%s'", filepath);
     fp = tmp_fp;
   }
