6c9be4a5a1dac2bcf3dd82bbbc979e0f663c0f5d
tracer-ics2022 <tracer@njuics.org>
2022-11-08 15:30:13 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:30:13 up  1:11,  5 users,  load average: 1.14, 0.54, 0.46

--- nemu/src/monitor/ftrace.c
+++ nemu/src/monitor/ftrace.c
@@ -75,10 +75,10 @@ void ftrace_record(char *p) {
   char tmp[5], tmp1[4];
   memcpy(tmp, p, 4 * sizeof(p[0]));
   printf("tmp: %s\n", tmp);
-  if (strcmp(tmp, "call") == 1) printf("1111\n");
+  if (strcmp(tmp, "call") == 0) printf("1111\n");
   else {
     memcpy(tmp1, tmp, 3);
-    if (strcmp(tmp1, "jal") == 1) printf("2222\n");
+    if (strcmp(tmp1, "jal") == 0) printf("2222\n");
   }
 }
 
