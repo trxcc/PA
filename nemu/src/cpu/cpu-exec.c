6982905324e77d6720362584f4ac53cab92dfed6
tracer-ics2022 <tracer@njuics.org>
2022-11-08 14:33:00 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:33:00 up 14 min,  4 users,  load average: 0.09, 0.18, 0.21

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -80,20 +80,24 @@ static void exec_once(Decode *s, vaddr_t pc) {
   //assert(0);
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
+  printf("1: %s\n", p);
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
+  printf("2: %s\n", p);
   int ilen = s->snpc - s->pc;
   int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
+  printf("3: %s\n", p);
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
   if (space_len < 0) space_len = 0;
   space_len = space_len * 3 + 1;
   memset(p, ' ', space_len);
   p += space_len;
-  
+  printf("4: %s\n", p); 
+   
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
