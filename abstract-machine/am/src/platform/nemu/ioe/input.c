5c3775f9a30029f8ce7c4931668da0d640c1cea1
tracer-ics2022 <tracer@njuics.org>
2022-11-17 19:32:13 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:32:13 up  1:02, 11 users,  load average: 0.03, 0.01, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,7 +5,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 1;
-  kbd->keycode = inl(KBD_ADDR);
+  uint8_t key = inl(KBD_ADDR);
+  kbd->keydown = key & KEYDOWN_MASK ? true : false;
+  kbd->keycode = key & ~KEYDOWN_MASK; 
   printf("keycode: %d\n", kbd->keycode);
 }
