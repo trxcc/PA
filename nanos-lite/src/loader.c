1090ae2c5f387b75798b2f1836170c9312aba542
tracer-ics2022 <tracer@njuics.org>
2022-12-12 23:01:37 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:01:37 up  4:33,  7 users,  load average: 0.01, 0.01, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,11 +10,20 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+#if defined(__ISA_AM_NATIVE__)
+#define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
+#define EXPECT_TYPE EM_RISCV
+#elif
+#error unsupported ISA __ISA__
+#endif
+
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+
   void *Elf32_File = malloc(get_ramdisk_size());
   ramdisk_read(Elf32_File, 0, get_ramdisk_size());
   assert(Elf32_File != NULL);
