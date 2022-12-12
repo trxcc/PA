c4313231bef1ad0906b9a135937e70a34c31076f
tracer-ics2022 <tracer@njuics.org>
2022-12-13 01:06:50 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:06:50 up  6:38,  9 users,  load average: 0.18, 0.11, 0.03

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,14 +1,18 @@
 #include <common.h>
 #include "syscall.h"
 
-static void sys_yield() {
+extern void yield();
+
+static int sys_yield() {
   printf("what\n");
+  yield();
+  return 0;
 }
 
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf("a[0]: %u", a[0]);
+  //printf("a[0]: %u", a[0]);
   switch (a[0]) {
     case 1: sys_yield(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
