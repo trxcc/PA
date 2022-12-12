6c6cf56c2b687762a38aabcb5d94a2d7ff639a42
tracer-ics2022 <tracer@njuics.org>
2022-12-13 01:14:29 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:14:29 up  6:46,  9 users,  load average: 0.04, 0.04, 0.01

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,7 +14,7 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
   //printf("a[0]: %u", a[0]);
   switch (a[0]) {
-    case 1: sys_yield(); break;
+    case 1: c->GPRx = sys_yield(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
