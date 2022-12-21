bf2adc96b44cd06b5a6f703d8a61d59776cafb91
tracer-ics2022 <tracer@njuics.org>
2022-12-21 16:51:35 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:51:35 up  7:20,  6 users,  load average: 1.21, 1.33, 1.23

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -56,16 +56,17 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 #define W 400
 #define H 300
-static uint32_t color_buf[W * H];
+//static uint32_t color_buf[W * H];
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  assert(offset <= W * H);
+  assert(offset <= W * H * 4);
+  offset /= 4;
   int y = offset / W, x = offset - y * W;
   uint32_t *buff = (uint32_t *)buf;
-  for (size_t i = 0; i < len; i++) {
+  /*for (size_t i = 0; i < len; i++) {
     color_buf[y * W + x + i] = buff[i];
-  }
-  io_write(AM_GPU_FBDRAW, x, y, color_buf, len, 1, true);
+  }*/
+  io_write(AM_GPU_FBDRAW, x, y, buff, len, 1, true);
   return len;
 }
 
