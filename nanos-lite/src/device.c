581f6c3b7f462a1ba226592a973a934cf3196107
tracer-ics2022 <tracer@njuics.org>
2022-12-21 16:58:19 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:58:19 up  7:27,  6 users,  load average: 1.27, 1.30, 1.26

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -61,8 +61,8 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   assert(offset <= W * H * 4);
   offset /= 4;
-  printf("offset: %d\n", offset);
   int y = offset / W, x = offset - y * W;
+  printf("offset: %d, x: %d, y: %d\n", offset, x, y);
   uint32_t *buff = (uint32_t *)buf;
   /*for (size_t i = 0; i < len; i++) {
     color_buf[y * W + x + i] = buff[i];
