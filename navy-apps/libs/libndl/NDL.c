1c9855caf2e03864662bba8ac3a5356ecf6dda28
tracer-ics2022 <tracer@njuics.org>
2022-12-21 23:42:40 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:42:40 up 56 min,  3 users,  load average: 0.17, 0.16, 0.21

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -66,7 +66,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 }
 
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  //printf("x: %d, y: %d, w: %d, h: %d\n", x, y, w, h);
+  printf("x: %d, y: %d, w: %d, h: %d\n", x, y, w, h);
   FILE* fp = fopen("/dev/fb", "r+");
   assert(x + w < 400);
   assert(y + h < 300);
