7ef68b31a7accde4b1a677b49e3ef74432011c56
tracer-ics2022 <tracer@njuics.org>
2022-12-22 12:01:40 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:01:40 up  1:16,  5 users,  load average: 0.43, 0.19, 0.16

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,7 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <assert.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -12,11 +14,44 @@ int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
+char key_buf[64], *key, *act;
+
+static int ana_key(uint8_t *type, uint8_t *sym) {
+  if (!NDL_PollEvent(key_buf, sizeof(key_buf))) {
+    return 0;
+  }
+  act = key_buf;
+  int i;
+  for (i = 0; key_buf[i] != ' '; i++);
+  key_buf[i] = '\0';
+  key = key_buf + i + 1;
+  
+  for (i = 0; key[i] != '\0' && key[i] != '\n'; i++);
+  if (key[i] == '\n') key[i] = '\0';
+
+  if (act[1] == 'd') *type = SDL_KEYDOWN;
+  else *type = SDL_KEYUP;
+
+  for (i = 0; i < sizeof(keyname) / sizeof(char *); i++) {
+    if (strcmp(key, keyname[i]) == 0) {
+      *sym = i;
+      return 1;
+    }
+  }
+  assert(0);
+  return 1;
+}
+
 int SDL_PollEvent(SDL_Event *ev) {
   return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  uint8_t tmp_type = 0, tmp_sym = 0;
+  while (!ana_key(&tmp_type, &tmp_sym));
+  event->type = tmp_type;
+  event->key.keysym.sym = tmp_sym;
+  
   return 1;
 }
 
