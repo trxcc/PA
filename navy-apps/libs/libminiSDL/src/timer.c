77d36367af9c561a409f9b214a0918227390ba47
tracer-ics2022 <tracer@njuics.org>
2022-12-22 19:12:38 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:12:38 up  8:27,  5 users,  load average: 0.42, 1.13, 1.32

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -18,6 +18,6 @@ uint32_t SDL_GetTicks() {
 }
 
 void SDL_Delay(uint32_t ms) {
-//  uint32_t start = NDL_GetTicks();
-//  while(NDL_GetTicks() - start <= ms / 1000000);
+  uint32_t start = NDL_GetTicks();
+  while(NDL_GetTicks() - start <= ms / 1000);
 }
