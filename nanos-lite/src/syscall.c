7149bfda30f7703f99764ed14c591b8be920ee9c
tracer-ics2022 <tracer@njuics.org>
2022-12-22 22:13:29 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:13:29 up 11:28,  5 users,  load average: 0.41, 0.45, 0.43

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -111,8 +111,7 @@ void do_syscall(Context *c) {
       c->GPRx = sys_gettimeofday((struct timeval *)a[1], (struct timezone *)a[2]); 
       break;
     case SYS_execve:
-      printf("%s\n", (char *)a[0]);
-      c->GPRx = sys_execve((char *)a[0], (char * const*)a[1], (char * const*)a[2]);
+      c->GPRx = sys_execve((char *)a[1], (char * const*)a[2], (char * const*)a[3]);
       break;
     case SYS_exit: 
 #ifdef CONFIG_STRACE
