f88e9a1256ac84cc4e211520b8ba3344104b47a6
tracer-ics2022 <tracer@njuics.org>
2022-12-22 11:19:08 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:19:08 up 33 min,  5 users,  load average: 0.26, 0.20, 0.20

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -86,8 +86,9 @@ int fs_open(const char *pathname, int flags, int mode) {
 
 size_t fs_read(int fd, void *buf, size_t len) {
   if (fd >= 0 && fd <= SPECIAL_FILE_NUM) {
+    size_t i = file_table[fd].read(buf, file_state[fd].open_offset, len);
     file_state[fd].open_offset += len;
-    return file_table[fd].read(buf, file_state[fd].open_offset, len);
+    return i;
   }
   int check_flag = check_open_overflow(fd, len);
   if (check_flag == -1) {
@@ -105,8 +106,9 @@ size_t fs_read(int fd, void *buf, size_t len) {
 
 size_t fs_write(int fd, const void *buf, size_t len) {
   if (fd >= 0 && fd <= SPECIAL_FILE_NUM) {
+    size_t i = file_table[fd].write(buf, file_state[fd].open_offset, len);
     file_state[fd].open_offset += len;
-    return file_table[fd].write(buf, file_state[fd].open_offset, len);
+    return i;
   }
   int check_flag = check_open_overflow(fd, len);
   if (check_flag == -1) {
