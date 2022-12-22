6e5073ff302d00cc1fb38f4baf14a5d120ab67f8
tracer-ics2022 <tracer@njuics.org>
2022-12-22 15:30:56 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:30:56 up  4:45,  5 users,  load average: 0.68, 0.61, 0.70

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -12,7 +12,18 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  FILE *fp = fopen(filename, "r+");
+  fseek(fp, 0, SEEK_END);
+  int size = (int)ftell(fp);
+  
+  char *buf = malloc(size * sizeof(char));
+  fseek(fp, 0, SEEK_SET);
+  fread(buf, size, 1, fp);
+
+  SDL_Surface *ret = STBIMG_LoadFromMemory(buf, size);
+  free(buf);
+  fclose(fp);
+  return ret;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -25,6 +25,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     if (!dstrect) dst_x = 0, dst_y = 0;
     else dst_x = dstrect->x, dst_y = dstrect->y;
   
+    printf("src_x: %d, src_y: %d, dst_x: %d, dst_y: %d, rect_w: %d, rect_h: %d\n", src_x, src_y, dst_x, dst_y, rect_w, rect_h);
+
     for (int i = 0; i < rect_h; i++)
       for (int j = 0; j < rect_w; j++) 
         dst_pix[(dst_y + i) * dst->w + dst_x + j] = src_pix[(src_y + i) * src->w + src_x + j];
