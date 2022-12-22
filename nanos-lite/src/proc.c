97cd7867992462dc113d71d31fe63192bac09ae2
tracer-ics2022 <tracer@njuics.org>
2022-12-22 20:18:37 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:18:37 up  9:33,  8 users,  load average: 0.15, 0.34, 0.81

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,7 @@ void init_proc() {
   switch_boot_pcb();
 
   Log("Initializing processes...");
-  naive_uload(NULL, "/bin/timer-test");
+  naive_uload(NULL, "/bin/bird");
   // load program here
 
 }
