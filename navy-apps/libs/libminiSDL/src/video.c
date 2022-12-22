d15bb335409ec8c4034b81e57529e401279ab3d9
tracer-ics2022 <tracer@njuics.org>
2022-12-22 20:27:59 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:27:59 up  9:42,  8 users,  load average: 0.53, 0.91, 0.95

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -120,7 +120,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
     for (int i = 0; i < h; i++) 
       for (int j = 0; j < w; j++) { 
         pix_arg[i * w + j] = get32color(&s->format->palette->colors[p[(y + i) * s->w + x + j]]);
-        if (pix_arg[i*w + j]) printf("pixels[%d] = %u\n", i*w+j, pix_arg[i*w+j]);
+//        if (pix_arg[i*w + j]) printf("pixels[%d] = %u\n", i*w+j, pix_arg[i*w+j]);
       }
     NDL_DrawRect(pix_arg, x, y, w, h);
   }
