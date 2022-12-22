793460730120e891acc86be2dd3b81f05748b093
tracer-ics2022 <tracer@njuics.org>
2022-12-22 17:36:44 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:36:44 up  6:51,  5 users,  load average: 0.87, 0.66, 0.56

--- navy-apps/libs/libminiSDL/src/general.c
+++ navy-apps/libs/libminiSDL/src/general.c
@@ -1,5 +1,6 @@
 #include <NDL.h>
 #include <assert.h>
+#include <stdio.h>
 
 int SDL_Init(uint32_t flags) {
   return NDL_Init(flags);
@@ -23,5 +24,7 @@ int SDL_ShowCursor(int toggle) {
 }
 
 void SDL_WM_SetCaption(const char *title, const char *icon) {
-  assert(0);
+  title = "----------I'm pig in PA------------";
+  printf("No icons\n");
+  return;
 }
