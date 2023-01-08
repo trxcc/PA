3e6cd5a9e7abe235b408b98961123d71f7e4a5b7
tracer-ics2022 <tracer@njuics.org>
2023-01-08 14:31:57 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:31:57 up 5 min,  5 users,  load average: 0.32, 0.40, 0.21

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -36,6 +36,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *context = kstack.end - sizeof(Context);
+  context->mstatus = 0x1800;
   context->mepc = (uintptr_t)entry;
   context->pdir = NULL;
   printf("Hit kcontext, entry = %u!\n", context->mepc);
