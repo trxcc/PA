91e0b692d495a172e78fbef90eefe68171f0ffec
tracer-ics2022 <tracer@njuics.org>
2023-01-08 21:01:57 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:01:57 up  6:35,  7 users,  load average: 0.25, 0.36, 0.28

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -6,15 +6,15 @@ int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
-
+/*
   uintptr_t argc = *args;
   
   uintptr_t *tmp = args;
   char **argv = (char **)(tmp + 1);
   for (tmp = tmp + 1; *tmp; tmp++);
   char **envp = (char **)envp;
-
+*/
   environ = empty;
-  exit(main(argc, argv, envp));
+  exit(main(0, empty, empty));
   assert(0);
 }
