65a4cdae56dcd21c82c03ffa907c6472e3c898f4
tracer-ics2022 <tracer@njuics.org>
2023-01-09 18:32:06 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:32:06 up  7:58,  7 users,  load average: 0.39, 0.25, 0.09

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -33,8 +33,8 @@ void init_proc() {
 //  hello_fun(NULL);
   Log("Initializing processes...");
   //context_uload(&pcb[0], "/bin/hello");
-  char *argv[] = {"--skip", NULL};
-  context_uload(&pcb[1], "/bin/pal", argv, NULL);
+  //char *argv[] = {"--skip", NULL};
+  context_uload(&pcb[1], "/bin/exec-test", NULL, NULL);
 //  assert(0);
 //  naive_uload(NULL, "/bin/pal");
   switch_boot_pcb();
