b6df034689cd9acf71cd5c98faac4fd66f9ad6a8
tracer-ics2022 <tracer@njuics.org>
2023-01-10 17:44:08 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:44:08 up  4:42,  4 users,  load average: 0.30, 0.71, 1.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -30,7 +30,7 @@ extern size_t fs_lseek(int, size_t, int);
 extern int fs_close(int);
 extern size_t fs_get_file_size(int);
 extern size_t fs_get_file_off(int);
-
+/*
 static uintptr_t loader(PCB *pcb, const char *filename) {
   printf("filename: %s\n", filename);
   int fd = fs_open(filename, 0, 0);
@@ -73,6 +73,33 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //printf("0x%u\n", ehdr->e_entry);
   return ehdr->e_entry;
 }
+*/
+
+static uintptr_t loader(PCB *pcb, const char *filename) {
+  int fd = fs_open(filename, 0, 0);
+  
+  Elf_Ehdr ehdr;
+  fs_lseek(fd, 0, SEEK_SET);
+  fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
+
+  assert(&ehdr != NULL);
+  assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
+  assert(ehdr.e_machine = EXPECT_TYPE);
+
+  for (int i = 0; i < ehdr.e_phnum; i++) {
+    Elf_Phdr phdr;
+    fs_lseek(fd, ehdr.e_phoff + i * ehdr.e_phentsize, SEEK_SET);
+    fs_read(fd, &phdr, ehdr.e_phentsize);
+    if (phdr.p_type == PT_LOAD) {
+      printf("PT_LOAD good!\n");
+      fs_lseek(fd, phdr.p_offset, SEEK_SET);
+      fs_read(fd, (void *)phdr.p_vaddr, phdr.p_filesz);
+      memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+    }
+  }
+  printf("e_entry: %u\n", ehdr.e_entry);
+  return ehdr.e_entry;
+}
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
@@ -97,10 +124,10 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
     for (; envp[envc]; envc++);
   }
 
-  //void *now_page_head = new_page(NR_PAGE) + (NR_PAGE << 12);
-  //printf("now_page_head: %u\n", (uintptr_t)now_page_head); 
-  char *us_pointer = (char *)heap.end;
-  //char *us_pointer = (char *)now_page_head;
+  void *now_page_head = new_page(NR_PAGE) + (NR_PAGE << 12);
+  printf("now_page_head: %u\n", (uintptr_t)now_page_head); 
+  //char *us_pointer = (char *)heap.end;
+  char *us_pointer = (char *)now_page_head;
   uintptr_t *us_argv[argc + 1];
   printf("argv start----\n");
 
@@ -142,8 +169,8 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   uintptr_t entry = loader(pcb, filename);
 
   Area area;
-  area.start = &pcb->cp;
-  area.end = &pcb->cp + STACK_SIZE;
+  area.start = (void *)pcb->stack;
+  area.end = (void *)(pcb->stack + STACK_SIZE);
   pcb->cp = ucontext(NULL, area, (void *)entry);
   pcb->cp->GPRx = (uintptr_t)addrptr;
   Log("In uload, entry = %u, pcb->cp->mepc = %u", entry, pcb->cp->mepc);
@@ -151,8 +178,8 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 
 void context_kload(PCB *pcb, void (*entry)(void *), void *arg) {
   Area area;
-  area.start = &pcb->cp;
-  area.end = &pcb->cp + STACK_SIZE;
+  area.start = (void *)pcb->stack;
+  area.end = (void *)(pcb->stack + STACK_SIZE);
   pcb->cp = kcontext(area, entry, arg);
   Log("In kload, entry = %u", entry);
 }
