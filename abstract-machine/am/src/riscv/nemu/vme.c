87e76423d5838ab951e3322386de92f8f70f25a1
tracer-ics2022 <tracer@njuics.org>
2023-01-11 18:28:41 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:28:41 up  7:54,  5 users,  load average: 1.42, 0.74, 0.38

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -73,11 +73,11 @@ void __am_switch(Context *c) {
 #define PPN_MASK 0xfffffc00L
 
 void map(AddrSpace *as, void *va, void *pa, int prot) {
-  printf("as->ptr: %u, va: %u, pa: %u\n", (uintptr_t)as->ptr, (uintptr_t)va, (uintptr_t)pa);
   PTE* pte = as->ptr + VA_PPN_1(va) * 4;
-  printf("pte: %u, *pte: %u\n", (uintptr_t)pte, (uint32_t)*pte);
   if (!(*pte & PTE_V)) {
     printf("pte not valid\n");
+  printf("as->ptr: %u, va: %u, pa: %u\n", (uintptr_t)as->ptr, (uintptr_t)va, (uintptr_t)pa);
+  printf("pte: %u, *pte: %u\n", (uintptr_t)pte, (uint32_t)*pte);
     void *new_alloc_page = pgalloc_usr(PGSIZE);
     //Sv32 has a pa with 34 bits, right shift 2 bits before getting the PPN
     *pte = (*pte & ~PPN_MASK) | (PPN_MASK & ((uintptr_t)new_alloc_page >> 2));
@@ -86,7 +86,7 @@ void map(AddrSpace *as, void *va, void *pa, int prot) {
   
   PTE *leaf_pte = (PTE *)((((*pte & PPN_MASK) >> 10) << 12) + VA_PPN_0(va) * 4);
   *leaf_pte = (PPN_MASK & (uintptr_t)pa >> 2) | PTE_V | PTE_R | PTE_W | PTE_X;
-  printf("leaf_pte: %u, *leaf_pte: %u\n", leaf_pte, (uint32_t)*leaf_pte);
+  //printf("leaf_pte: %u, *leaf_pte: %u\n", leaf_pte, (uint32_t)*leaf_pte);
 }
 
 Context *ucontext(AddrSpace *as, Area kstack, void *entry) {
