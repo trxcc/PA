311498240f15bf1e83a0e86333548067fc6036a5
tracer-ics2022 <tracer@njuics.org>
2023-01-11 18:33:30 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:33:30 up  7:59,  5 users,  load average: 0.43, 0.46, 0.35

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -5,16 +5,16 @@ static void *pf = NULL;
 #define PAGE_SIZE (1 << 12)
 
 void* new_page(size_t nr_page) {
-  printf("new_page_pf: %u\n", (uintptr_t)pf);
+  //printf("new_page_pf: %u\n", (uintptr_t)pf);
   void *tmp = pf;
   pf += (nr_page << 12);
-  printf("tmp: %u, pf: %u\n", (uintptr_t)tmp, (uintptr_t)pf);
+  //printf("tmp: %u, pf: %u\n", (uintptr_t)tmp, (uintptr_t)pf);
   return tmp;
 }
 
 #ifdef HAS_VME
 static void* pg_alloc(int n) {
-  printf("In pg_alloc, n = %d\n", n);
+  //printf("In pg_alloc, n = %d\n", n);
   assert(n % PAGE_SIZE == 0);
   void *page_head = new_page(n / PAGE_SIZE);
   memset(page_head, 0, n);
