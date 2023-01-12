c0449c74657c71987f3f995a23702a51a569a83b
tracer-ics2022 <tracer@njuics.org>
2023-01-12 18:28:26 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:28:26 up 40 min,  5 users,  load average: 1.88, 1.64, 0.83

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#include <stdio.h>
+#include <assert.h>
 
 #ifdef __ISA_NATIVE__
 #error can not support ISA=native
@@ -9,6 +9,6 @@
 extern intptr_t _syscall_(intptr_t, intptr_t, intptr_t, intptr_t);
 
 int main() {
-  printf("In dummy\n");
+  assert(0);
   return _syscall_(SYS_yield, 0, 0, 0);
 }
