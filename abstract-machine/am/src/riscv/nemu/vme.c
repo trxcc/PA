324fb87b3d418cc80414e6450d62853c77b34443
tracer-ics2022 <tracer@njuics.org>
2023-01-15 15:15:17 +0800

    >  compile NEMU 211300082 谭荣熙 Linux trxcc-virtual-machine 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:15:17 up  5:34,  5 users,  load average: 0.22, 0.11, 0.04

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -57,7 +57,7 @@ void unprotect(AddrSpace *as) {
 }
 
 void __am_get_cur_as(Context *c) {
-  c->pdir = (vme_enable ? (void *)get_satp() : NULL);
+  c->pdir = (vme_enable && c->pdir != NULL ? (void *)get_satp() : NULL);
   printf("__am_get_cur_as:\tc->pdir=%u\n", c->pdir);
 }
 
